30f2f0b2e5f56afce3c479a79776ec363a5e0dc2
gshperera <pereragsh123@gmail.com>
2026-03-10 12:10:12 +0530

    feat: create mcp server to manage inventory(csv)

---
+++ main.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+# from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+import asyncio
+
+load_dotenv()
+
+MCP_SERVERS = {
+    "file-service": {
+        "transport": "stdio",  # Since we run all things (ex: servers) locally
+        "command": "uv",
+        "args": ["run", "mcp_file_service.py"],
+        "cwd": ".",
+    }
+}
+
+
+async def run_chat():
+    client = MultiServerMCPClient(MCP_SERVERS)
+
+    tools = await client.get_tools()
+
+    model = ChatOpenAI(model="gpt-4.1-mini")
+    agent = create_agent(model, tools)
+
+    while True:
+        user_text = input("You: ").strip()
+
+        result = await agent.ainvoke(
+            {"messages": [{"role": "user", "content": user_text}]}
+        )
+
+        assistant_text = result["messages"][-1].content
+        print(f"AI: {assistant_text}\n")
+
+
+if __name__ == "__main__":
+    asyncio.run(run_chat())
